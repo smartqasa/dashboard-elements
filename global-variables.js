0a5f9ee7486ec5dc35dbc7edfcdaf210bfb7d628
Tom Bruno <tom.bruno@smartqasa.com>
2024-02-09 09:59:50 -0500

    Create global-variables.js

---
+++ global-variables.js
@@ -0,0 +1,11 @@
+// SmartQasa - Set global variable for Device type
+window.smartqasa = window.smartqasa || {};
+
+// Determine the device type based on screen width
+window.smartqasa.deviceType = 
+    typeof window.screen.width !== "number" 
+    ? 'tablet' 
+    : (window.screen.width < 600 ? 'phone' : 'tablet');
+
+// Reset Home Path
+// window.smartqasa.homePath = 'notSet';
